bd2e6896f9e020018d72fb4dfa22f9185a532719
Thomas Fuchs-Martin <t.fuchsmartin@gmail.com>
2018-03-03 23:25:49 +0700

    remove no longer used api call methods

--- app/src/main/java/com/appham/projectviewer/MainActivity.java
+++ app/src/main/java/com/appham/projectviewer/MainActivity.java
@@ -21,19 +21,6 @@ public class MainActivity extends AppCompatActivity {
         ProjectListFragment projectListFragment = new ProjectListFragment();
         getFragmentManager().beginTransaction().add(R.id.frameProjects, projectListFragment, ProjectListFragment.TAG)
                 .commit();
-
-        callAPI();
     }
 
-    private void callAPI() {
-        ProjectsApi projectsApi = ApiFactory.createProjectsApi();
-        projectsApi.getProjects().subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(this::logResult, Throwable::printStackTrace);
-    }
-
-    private int logResult(ProjectsList p) {
-        return Log.i("subscribe", p+"");
-
-    }
 }
